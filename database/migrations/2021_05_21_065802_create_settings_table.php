187655705714805032047eb3ae2a88485fe1306e
PritamNandy <prasun.nandy@northsouth.edu>
2022-07-31 12:09:51 +0600

    new fixes

--- database/migrations/2021_05_21_065802_create_settings_table.php
+++ database/migrations/2021_05_21_065802_create_settings_table.php
@@ -15,9 +15,9 @@ class CreateSettingsTable extends Migration
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('logo');
-            $table->string('favicon');
+            $table->string('title')->nullable();
+            $table->string('logo')->nullable();
+            $table->string('favicon')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2021_05_21_065848_add_role_id_column_to_users_table.php
+++ database/migrations/2021_05_21_065848_add_role_id_column_to_users_table.php
@@ -14,7 +14,9 @@ class AddRoleIdColumnToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-
+            $table->foreignId('role_id')
+            ->references('id')->on('roles')
+            ->onDelete('cascade');
         });
     }
 
